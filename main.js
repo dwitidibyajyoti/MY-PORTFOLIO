d09be61665552340b80606be114c37ebb9b65014
dwitidibyajyoti sahoo <dwitidibyajyoti@gmail.com>
2020-11-19 23:45:25 +0530

    small change in gsap animation on home page

--- main.js
+++ main.js
@@ -57,6 +57,7 @@ function goDown() {
 const timeline = gsap.timeline({defaults: {duration: 2}});
 timeline
   .from('.logo', {x: 300, ease: 'circ', delay: 0.5, opacity: 0})
+  .from('.nav-itom', {x: 300, ease: 'circ', opacity: 0}, 0)
   .from('.tex', {x: -300, ease: 'circ', delay: 0.5, opacity: 0}, 1)
   .from('.home-svg', {x: 300, ease: 'circ', opacity: 0}, 1)
   .from('.circle-one', {y: -700, duration: 1, ease: 'bounce', opacity: 0})
